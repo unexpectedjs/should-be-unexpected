5058edb2f292e1c188048e5127a3c4c0b788a3a2
Gustav Nikolaj Olsen <gno@one.com>
2015-01-08 23:22:45 +0100

    checkpoint

--- index.js
+++ index.js
@@ -1,13 +1,54 @@
 var unexpected = require('unexpected');
 
-function ShouldFacade() {
-
+function expandFlags(pattern, flags) {
+    return pattern.replace(/\[(!?)([^\]]+)\] ?/g, function (match, negate, flag) {
+        return Boolean(flags[flag]) !== Boolean(negate) ? flag + ' ' : '';
+    }).trim();
 }
 
-
-
-function should() {
-
+function defineProps(object, props) {
+	props.forEach(function (prop) {
+		if (typeof prop === 'object') {
+			Object.defineProperty(object, prop.name, { get: prop.method });
+		} else {
+			Object.defineProperty(object, prop, {
+				get: function () {
+					return object;
+				}
+			});
+		}
+	});
 }
+
+function ShouldFacade(subject) {
+	var that = this;
+
+	this.subject = subject;
+	this.negate = false;
+
+	defineProps(this, [
+		'to',
+		'an',
+		'a',
+		'have',
+
+	]);
+}
+
+ShouldFacade.prototype.be = function (value) {
+	var assertion = expandFlags('[not] to be', { 'not': this.negate });
+	unexpected(this.subject, assertion, value)
+	return this;
+};
+
+function should(subject) {
+	return new ShouldFacade(subject);
+}
+
+Object.defineProperty(Object.prototype, 'should', {
+	get: function () {
+		return new ShouldFacade(this);
+	}
+})
 
 module.exports = should;
--- test/basic.js
+++ test/basic.js
@@ -6,4 +6,9 @@ describe('basic', function () {
     it('should export a function', function () {
         expect(should, 'to be a function');
     });
+    it('foo.should.be(bar)', function () {
+    	should(3).be(3);
+		should(3).be(3);
+    	should(3).not.be(3);
+    });
 });
