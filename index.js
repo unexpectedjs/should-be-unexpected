de21d18ec8846c6e736cfa95f9362590fe0cd1d3
Gustav Nikolaj Olsen <gno@one.com>
2015-01-11 22:30:51 +0100

    index: fix style issues

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ var ShouldFacade = require('./lib/ShouldFacade');
 var extend = require('./lib/util/extendObject');
 
 function should(subject) {
-	return new ShouldFacade(subject);
+    return new ShouldFacade(subject);
 }
 
 extend(should, ShouldFacade.prototype);
 
 Object.defineProperty(Object.prototype, 'should', {
-	get: function () {
-		var value = this;
-		if (value instanceof Number || value instanceof String || value instanceof Boolean) {
-			value = value.valueOf();
-		}
-		return new ShouldFacade(value);
-	}
+    get: function () {
+        var value = this;
+        if (value instanceof Number || value instanceof String || value instanceof Boolean) {
+            value = value.valueOf();
+        }
+        return new ShouldFacade(value);
+    }
 });
 
 module.exports = should;
