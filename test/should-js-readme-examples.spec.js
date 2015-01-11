4c877344ee51381f60c4c206ba29086400c93cb2
Gustav Nikolaj Olsen <gno@one.com>
2015-01-11 22:10:20 +0100

    add tests for true and false - and some accidental indentation changes

--- test/should-js-readme-examples.spec.js
+++ test/should-js-readme-examples.spec.js
@@ -1,6 +1,12 @@
 var expect = require('unexpected');
 var should = require('../');
 
+/* These tests are adapted from the examples in the should.js readme.
+ * Negative tests are added to make sure they throw when failing,
+ * for completeness sake. The additions are marked with an asterisk (*)
+ * at the end of the name.
+ */
+
 describe('should.js readme:', function () {
     describe('Example:', function () {
         it('first example', function () {
@@ -75,6 +81,37 @@ describe('should.js readme:', function () {
             it('In order to test for null', function () {
                 var err = null;
                 (err === null).should.be.ok;
+            });
+            it('false.should.be.ok should throw *', function () {
+                expect(function () {
+                    false.should.be.ok;
+                }, 'to throw', 'expected false to be ok');
+            });
+        });
+        describe('.true', function () {
+            it('true.should.be.true', function () {
+                true.should.be.true;
+            });
+            it("'1'.should.not.be.true", function () {
+                '1'.should.not.be.true;
+            });
+            it('false.should.be.true should throw *', function () {
+                expect(function () {
+                    false.should.be.true;
+                }, 'to throw');
+            });
+        });
+        describe('.false', function () {
+            it('false.should.be.false', function () {
+                false.should.be.false;
+            });
+            it('(0).should.not.be.false', function () {
+                (0).should.not.be.false;
+            });
+            it('true.should.be.false should throw *', function () {
+                expect(function () {
+                    true.should.be.false;
+                }, 'to throw');
             });
         });
     });
