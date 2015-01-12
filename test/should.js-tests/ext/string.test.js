b3a07bbef89915cb444b3ad7049a6dbacae379f3
Gustav Nikolaj Olsen <gno@one.com>
2015-01-13 00:50:25 +0100

    should.js tests: string: mostly running

--- test/should.js-tests/ext/string.test.js
+++ test/should.js-tests/ext/string.test.js
@@ -8,19 +8,25 @@ describe('string', function() {
 
     err(function() {
       'foobar'.should.startWith('bar');
-    }, "expected 'foobar' to start with 'bar'");
+    }, "expected 'foobar' to match /^bar/");
+    // }, "expected 'foobar' to start with 'bar'");
 
     err(function() {
       'foobar'.should.not.startWith('foo');
-    }, "expected 'foobar' not to start with 'foo'");
+    }, "expected 'foobar' not to match /^foo/\n\nfoobar");
+    // }, "expected 'foobar' not to start with 'foo'");
 
+    // Incompatibility! Custom Error messages.
     err(function() {
       'foobar'.should.startWith('bar', 'baz');
-    }, "baz");
+    }, "expected 'foobar' to match /^bar/");
+    // }, "baz");
 
+    // Incompatibility! Custom Error messages.
     err(function() {
       'foobar'.should.not.startWith('foo', 'baz');
-    }, "baz");
+    }, "expected 'foobar' not to match /^foo/\n\nfoobar");
+    // }, "baz");
   });
 
   it('test endWith()', function() {
@@ -29,19 +35,25 @@ describe('string', function() {
 
     err(function() {
       'foobar'.should.endWith('foo');
-    }, "expected 'foobar' to end with 'foo'");
+    }, "expected 'foobar' to match /foo$/");
+    // }, "expected 'foobar' to end with 'foo'");
 
     err(function() {
       'foobar'.should.not.endWith('bar');
-    }, "expected 'foobar' not to end with 'bar'");
+    }, "expected 'foobar' not to match /bar$/\n\nfoobar");
+    // }, "expected 'foobar' not to end with 'bar'");
 
+    // Incompatibility! Custom Error messages.
     err(function() {
       'foobar'.should.endWith('foo', 'baz');
-    }, "baz");
+    }, "expected 'foobar' to match /foo$/");
+    // }, "baz");
 
+    // Incompatibility! Custom Error messages.
     err(function() {
       'foobar'.should.not.endWith('bar', 'baz');
-    }, "baz");
+    }, "expected 'foobar' not to match /bar$/\n\nfoobar");
+    // }, "baz");
   });
 
 });
