0eb3ca84e09aed5fd7068c28fefd2fa9f0cb64cb
Gustav Nikolaj Olsen <gno@one.com>
2015-01-12 23:26:25 +0100

    add should.js test suite

---
+++ test/should.js-tests/ext/eql.test.js
@@ -0,0 +1,73 @@
+var err = require('../util').err,
+  should = require('../../../');
+
+describe('eql', function() {
+  it('test eql(val)', function() {
+    'test'.should.eql('test');
+    ({foo: 'bar'}).should.eql({foo: 'bar'});
+    (1).should.eql(1);
+    var memo = [];
+
+    function memorize() {
+      memo.push(arguments);
+    }
+
+    memorize('a', [1, 2]);
+    memorize('a', [1, 2]);
+    memo[0].should.eql(memo[1]);
+
+    err(function() {
+      (4).should.eql(3);
+    }, 'expected 4 to equal 3');
+  });
+
+  it('test equal(val)', function() {
+    'test'.should.equal('test');
+    (1).should.equal(1);
+
+    err(function() {
+      (4).should.equal(3);
+    }, 'expected 4 to be 3');
+
+    err(function() {
+      '4'.should.equal(4);
+    }, "expected '4' to be 4");
+
+    var date = new Date;
+    date.should.equal(date);
+  });
+
+  it('test .equal()', function() {
+    var foo;
+    should.equal(undefined, foo);
+  });
+
+  it('should allow to test with prototypes', function() {
+    var b = {a: 2};
+    var a = Object.create(null);
+    a.a = 2;
+
+    b.should.be.eql(a);
+
+    should.config.checkProtoEql = true;
+
+    err(function() {
+      b.should.be.eql(a);
+    }, 'expected { a: 2 } to equal { a: 2 } (because A and B have different prototypes)');
+
+    should.config.checkProtoEql = false;
+  });
+
+  it('should allow to use old equal function', function() {
+    var a = { a: 10};
+    var b = { a: '10'};
+
+    a.should.be.not.eql(b);
+
+    should.config.useOldDeepEqual = true;
+
+    a.should.be.eql(b);
+
+    should.config.useOldDeepEqual = false;
+  });
+});
---
+++ test/should.js-tests/util.js
@@ -0,0 +1,25 @@
+var should = require('../../');
+
+function err(fn, msg) {
+  var ok = true;
+  try {
+    fn();
+    ok = false;
+  } catch (err) {
+    if((typeof msg == 'string' && err.message !== msg) || (msg instanceof RegExp && !msg.test(err.message))) {
+      throw new should.AssertionError({
+        message: [
+          'Expected message does not match',
+          'expected: ' + should.format(msg),
+          '  actual: ' + should.format(err.message)
+        ].join('\n'),
+        expected: msg,
+        actual: err.message,
+        stackStartFunction: err
+      });
+    }
+  }
+  if(!ok) throw new Error('expected an error');
+}
+
+exports.err = err;
