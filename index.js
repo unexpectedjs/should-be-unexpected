3adeb800af4986576c2ddac86162f680072825d2
Gustav Nikolaj Olsen <gno@one.com>
2015-01-13 22:25:25 +0100

    fix .exist on should function

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var ShouldFacade = require('./lib/ShouldFacade');
 var extend = require('./lib/util/extendObject');
+var expandFlags = require('./lib/util/expandFlags')
+var expect = require('unexpected');
 
 function should(subject) {
     return new ShouldFacade(subject);
@@ -8,6 +10,23 @@ function should(subject) {
 var uninspected = require('uninspected');
 uninspected.outputFormat = 'text';
 should.format = uninspected.inspect;
+
+Object.defineProperty(should, 'not', {
+	get: function () {
+		this.negate = !this.negate;
+		return this;
+	}
+})
+
+should.exist = function (value) { // 'to be null or undefined'
+	var not = !!this.negate;
+	this.negate = false;
+	if (not) {
+		return expect.it('to be null').or('to be undefined')(value);
+	} else {
+		return expect.it('not to be null').and('not to be undefined')(value);
+	}
+};
 
 extend(should, ShouldFacade.prototype);
 
